1f4b93883aed542ae9b2dc0a2f11e441ab13e9aa
LudoLaFayette <ludovicfaye89@gmail.com>
2022-10-24 19:11:28 +0200

    first commit

--- bdd.sql
+++ bdd.sql
@@ -1,6 +1,25 @@
 --
 -- code pour la création des tables
 --
+-- CREATE table montre(
+--     idMontre uuid primary key not null,
+--     libelleMontre varchar,
+--     ecranMontre varchar,
+--     boitierMontre varchar,
+--     braceletMontre varchar,
+--     materiauxBoitierMontre uuid,
+--     materiauxBraceletMontre uuid,
+--     commandeMontre bool,
+--     foreign key (materiauxBoitierMontre) references materiaux(idMateriaux),
+--     foreign key (materiauxBraceletMontre) references materiaux(idMateriaux)
+-- );
+-- CREATE table materiaux(
+--     idMateriaux uuid primary key not null,
+--     libelleMateriaux varchar,
+--     urlMateriaux varchar,
+--     idMontre uuid,
+--     foreign key (idMontre) references montre(idMontre)
+-- );
 
 
 
