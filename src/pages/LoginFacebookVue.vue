e5abca6127e811aa2cbc7e7a09028d6a620ecbe6
LudoLaFayette <ludovicfaye89@gmail.com>
2022-10-25 14:49:35 +0200

    mise à jour : ajout authetification tiers + MCD et readme MAJ

---
+++ src/pages/LoginFacebookVue.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import LoginFacebook from '@/components/LoginFacebook.vue'
+</script>
+<template>
+    <LoginFacebook/>
+</template>
---
+++ src/pages/LoginGoogleVue.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import LoginGoogle from '@/components/LoginGoogle.vue'
+</script>
+<template>
+    <LoginGoogle/>
+</template>
